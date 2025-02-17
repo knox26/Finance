fe0ab000309cd3d3a3ed21e3163e6f9fcb475e02
knox26 <bvjadhav7558@gmail.com>
2025-02-17 11:43:29 +0530

    modified:   backend/src/index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,17 +4,15 @@ import cors from "cors";
 import connectDB from "./lib/db-connect.js";
 import transactionRoutes from "./routes/transaction-routes.js";
 import path from "path";
+import http from "http";
+
 dotenv.config();
 const app = express();
+const port = process.env.PORT ;
+const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
-  });
-}
 
-const port = process.env.PORT || 3000;
+
 
 app.use(
   cors({
@@ -26,7 +24,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", transactionRoutes);
 
-app.listen(port, () => {
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
   connectDB();
 });
